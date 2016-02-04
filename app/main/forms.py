9e0c1c6b443fae171dff2dc0a3911d4ac79d0887
Waiyaki <muturijames3@gmail.com>
2016-02-04 06:38:45 +0300

    Allow Users to update maintenance requests
    Also allow normal users to view any maintenance
    requests that they may have that are still pending.
    
    Admins can update any request, maintainers can only
    update the requests assigned to them while
    users can only update the information they provided
    while filing the repair request.
    
    ** This does not handle the case where the maintainer
    has filed repair requests, which would require showing those
    requests alongside the requests assigned to that maintainer.

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,19 +1,70 @@
 from flask.ext.wtf import Form
 
-from wtforms import SubmitField, SelectField, StringField, TextAreaField
-from wtforms.validators import Required, Length
+from wtforms import SubmitField, SelectField, StringField, TextAreaField, BooleanField
+from wtforms.validators import Required, Length, ValidationError
+from wtforms_components import read_only
 
-from app.models import Facility
+from app.models import Facility, User, TaskStatus
 
 
-class TaskRequestForm(Form):
+class CommonTaskDetailsForm(Form):
     description = StringField("Description", validators=[Required(), Length(10, 255)])
     detailed_info = TextAreaField("Detailed Information")
-    facility = SelectField('Facility', coerce=int)
-    submit = SubmitField('Submit')
+    facility = SelectField("Facility", coerce=int)
 
     def __init__(self, *args, **kwargs):
-        super(TaskRequestForm, self).__init__(*args, **kwargs)
+        super(CommonTaskDetailsForm, self).__init__(*args, **kwargs)
         self.facility.choices = [
             (fac.id, fac.name) for fac in Facility.query.order_by(Facility.name).all()
         ]
+
+
+class TaskRequestForm(CommonTaskDetailsForm):
+    submit = SubmitField("Submit")
+
+    def __init__(self, *args, **kwargs):
+        super(TaskRequestForm, self).__init__(*args, **kwargs)
+
+
+class MaintainerTaskUpdateForm(CommonTaskDetailsForm):
+    acknowledged = BooleanField("Acknowledge Task Assignment Received")
+    progress = SelectField('Task Status', coerce=int)
+    submit = SubmitField("Update")
+
+    def __init__(self, *args, **kwargs):
+        super(MaintainerTaskUpdateForm, self).__init__(*args, **kwargs)
+        self.progress.choices = [
+            (TaskStatus.NOT_STARTED, "Not Started"),
+            (TaskStatus.STARTED, "Started"),
+            (TaskStatus.PENDING, "In Progress"),
+            (TaskStatus.DONE, "Done")
+        ]
+        read_only(self.facility)
+        read_only(self.description)
+        read_only(self.detailed_info)
+
+
+class AdminTaskUpdateForm(CommonTaskDetailsForm):
+    confirmed = BooleanField("Confirm Task Request")
+    assigned_to_id = SelectField("Assigned To", coerce=int)
+    acknowledged = BooleanField("Acknowledge Receipt by Assignee")
+    progress = SelectField("Task Status", coerce=int)
+    submit = SubmitField("Update")
+
+    def __init__(self, *args, **kwargs):
+        super(AdminTaskUpdateForm, self).__init__(*args, **kwargs)
+        self.assigned_to_id.choices = [
+            (u.id, u.username) for u in User.query.filter_by(
+                is_maintenance=True).order_by(User.username).all()
+        ]
+        self.progress.choices = [
+            (TaskStatus.NOT_STARTED, "Not Started"),
+            (TaskStatus.STARTED, "Started"),
+            (TaskStatus.PENDING, "In Progress"),
+            (TaskStatus.DONE, "Done")
+        ]
+
+    def validate_assigned_to_id(self, field):
+        id = field.data
+        if not User.query.filter_by(id=int(id), is_maintenance=True).first():
+            raise ValidationError("Selected choice is not a valid user.")
--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, redirect, abort
+from flask import render_template, url_for, redirect, abort, flash, request
 
 from flask.ext.login import login_required, current_user
 
@@ -6,14 +6,12 @@ from app import db
 from app.main import main
 from app.models import Task
 
-from app.main.forms import TaskRequestForm
+from app.main.forms import TaskRequestForm, MaintainerTaskUpdateForm, AdminTaskUpdateForm
 
 
 @main.route('/', methods=['GET', 'POST'])
 @login_required
 def index():
-    if not (current_user.is_admin or current_user.is_maintenance):
-        return redirect(url_for('main.task_request'))
     tasks = []
     if current_user.is_admin:
         tasks = Task.query.order_by(Task.date_requested.desc()).all()
@@ -21,6 +19,10 @@ def index():
     elif current_user.is_maintenance:
         assigned = current_user.assigned.all()
         return render_template('main/index.html', tasks=assigned)
+    else:
+        tasks = current_user.tasks.filter_by(
+            confirmed=False).order_by(Task.date_requested.desc()).all()
+        return render_template('main/index.html', tasks=tasks)
 
 
 @main.route('/task-requests', methods=['GET', 'POST'])
@@ -48,3 +50,66 @@ def view_task(task_id):
         if task.requested_by_id != current_user.id:
             abort(403)
     return render_template('main/task-detail.html', task=task)
+
+
+@main.route('/update-task/<int:task_id>', methods=['GET', 'POST'])
+@login_required
+def update_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    # Should we do this? Let's find out.
+    if not (current_user.is_admin or current_user.is_maintenance):
+        if task.requested_by_id != current_user.id:
+            abort(403)
+        if current_user.is_maintenance and task.assigned_to_id != current_user.id:
+            abort(403)
+
+    # Get the appropriate form for this user.
+    if current_user.is_admin:
+        form = AdminTaskUpdateForm()
+    elif current_user.is_maintenance:
+        form = MaintainerTaskUpdateForm()
+    else:
+        form = TaskRequestForm()
+
+    if request.method == "GET":
+        # Prepopulate the form fields with the correct data for a GET request.
+        if current_user.is_admin:
+            form.acknowledged.data = task.acknowledged
+            form.confirmed.data = task.confirmed
+            form.assigned_to_id.data = task.assigned_to_id
+            form.progress.data = task.progress
+        elif current_user.is_maintenance:
+            form.acknowledged.data = task.acknowledged
+            form.progress.data = task.progress
+        # Define form fields common to all forms.
+        form.description.data = task.description
+        form.detailed_info.data = task.detailed_info
+        form.facility.data = task.facility.id
+
+    elif request.method == "POST":
+        if form.validate_on_submit():
+            # Update data contained within specific fields according to user rights.
+            if current_user.is_admin:
+                task.acknowledged = form.acknowledged.data
+                task.confirmed = form.confirmed.data
+                task.assigned_to_id = form.assigned_to_id.data
+                task.progress = form.progress.data
+                task.facility_id = form.facility.data
+            elif current_user.is_maintenance:
+                if not form.acknowledged.data and form.progress.data != task.progress:
+                    # This guy updated progess but did not acknowledge receipt.
+                    # Let's kindly do that for them.
+                    form.acknowledged.data = True
+                task.acknowledged = form.acknowledged.data
+                task.progress = form.progress.data
+            # Common fields
+            task.description = form.description.data
+            task.detailed_info = form.detailed_info.data
+            db.session.add(task)
+            db.session.commit()
+            flash("Task update successful.")
+            return redirect(url_for('main.view_task', task_id=task.id))
+        else:
+            flash("Your form has some errors. Please correct them and try again.")
+
+    return render_template('main/task-update.html', task=task, form=form)
